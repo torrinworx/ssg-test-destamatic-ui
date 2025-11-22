0c8850dfb3b071ca1d21e594471ffa9c9e1bfd6b
Torrin Leonard <torrin@torrin.me>
2025-11-22 15:11:07 -0500

    Splitting frontend up for ssg

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,49 +1,5 @@
 import { mount } from 'destam-dom';
-import { Stage, StageContext, Button } from 'destamatic-ui';
 
-import About from './pages/about';
-import Blogs from './pages/blogs';
-import Landing from './pages/landing';
+import App from './App';
 
-
-const stageConfig = {
-	stages: {
-		Landing: Landing,
-		About: About,
-		Blogs: Blogs,
-	},
-	template: ({ children }) => children,
-};
-
-const Pages = StageContext.use(s => () => {
-	s.open({ name: 'Landing', label: 'Landing Page' });
-
-	return <>
-		<div theme='row'>
-			<Button
-				type="contained"
-				label="Landing"
-				onClick={() => s.open({ name: 'Landing' })}
-				style={{ marginRight: 8 }}
-			/>
-			<Button
-				type="contained"
-				label="Blogs"
-				onClick={() => s.open({ name: 'Blogs' })}
-				style={{ marginRight: 8 }}
-			/>
-			<Button
-				type="contained"
-				label="About"
-				onClick={() => s.open({ name: 'About' })}
-				style={{ marginRight: 8 }}
-			/>
-		</div>
-		<Stage />
-	</>;
-});
-
-const App = () => <StageContext value={stageConfig}>
-	<Pages />
-</StageContext>;
 mount(document.body, <App />);
---
+++ frontend/index.ssg.jsx
@@ -0,0 +1,6 @@
+import App from './App';
+import { renderToString } from 'destamatic-ui';
+
+export function renderAppToString() {
+  return renderToString(App);
+};

9af5e868b6f675b895b7b099004e38c0c268edaf
Torrin Leonard <torrin@torrin.me>
2025-11-28 13:45:20 -0500

    Adding comment

--- frontend/index.ssg.jsx
+++ frontend/index.ssg.jsx
@@ -1,6 +1,7 @@
 import App from './App';
 import { render } from 'destamatic-ui';
 
+// TODO: Make render => mount, single function for both ssg and regular app stuff.
 export function renderAppToString() {
   return render(App);
 };

e814369619bd1c4d0526d2a57258309f781f133b
Torrin Leonard <torrin@torrin.me>
2025-11-28 19:47:52 -0500

    Adding nested stage context in blogs

--- frontend/pages/blogs.jsx
+++ frontend/pages/blogs.jsx
@@ -1,10 +1,78 @@
-import { Typography } from "destamatic-ui"
+import { Stage, StageContext, Button } from 'destamatic-ui';
+
+
+const BlogHome = () => {
+    return <div>
+        <h2>Blog Home</h2>
+        <p>Welcome to the blog section. Choose a post using the buttons above.</p>
+    </div>;
+};
+
+const FirstPost = () => {
+    return <div>
+        <h2>First Post</h2>
+        <p>This is the first example blog post. You can put any content here.</p>
+    </div>;
+};
+
+const SecondPost = () => {
+    return <div>
+        <h2>Second Post</h2>
+        <p>This is the second example blog post. Yet another nested stage example.</p>
+    </div>;
+};
+
+const blogsStageConfig = {
+    stages: {
+        BlogHome: BlogHome,
+        FirstPost: FirstPost,
+        SecondPost: SecondPost,
+    },
+    template: ({ children }) => children,
+    initial: 'BlogHome',
+    ssg: true,
+    route: 'blogs', //TODO: Maybe routes are determined by the nested stages? Somehow? idk? something cleaner than this.
+};
+
+const BlogPages = StageContext.use(s => () => {
+    return <>
+        <div theme="row" style={{ marginBottom: 16 }}>
+            <Button
+                type="contained"
+                label="Blog Home"
+                onClick={() => {
+                    s.open({ name: 'BlogHome' });
+                }}
+                href="BlogHome.html"
+                style={{ marginRight: 8 }}
+            />
+            <Button
+                type="contained"
+                label="First Post"
+                onClick={() => {
+                    s.open({ name: 'FirstPost' });
+                }}
+                href="FirstPost.html"
+                style={{ marginRight: 8 }}
+            />
+            <Button
+                type="contained"
+                label="Second Post"
+                onClick={() => {
+                    s.open({ name: 'SecondPost' });
+                }}
+                href="SecondPost.html"
+                style={{ marginRight: 8 }}
+            />
+        </div>
+        <Stage />
+    </>;
+});
 
 const Blogs = () => {
-
-    return <>
-        <Typography label='Blogs page' />
-    </>;
+    return <StageContext value={blogsStageConfig}>
+        <BlogPages />
+    </StageContext>;
 };
 
 export default Blogs;

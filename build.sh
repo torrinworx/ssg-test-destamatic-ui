e7e763388122900fb86ecaf599d4693334b3bc9a
Torrin Leonard <torrin@torrin.me>
2025-11-30 16:46:23 -0500

    Adding build.sh script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+vite build
+vite build --config vite.config.ssg.js 
+node ./destamatic-ui/ssg/build.js ./build ./frontend
+npx esbuild ./server.js --bundle --platform=node --target=node23 --outfile=./build/index.js --format=esm --external:fs --external:path --external:os --external:crypto --external:vite --external:lightningcss
